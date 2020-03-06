d97549ee0e5d5fa16f7ccee4ec3eca3caec9528f
younu8084 <younu8084@gmail.com>
2020-03-06 18:32:47 +0530

    Update sonarMetrics.groovy

--- vars/sonarMetrics.groovy
+++ vars/sonarMetrics.groovy
@@ -7,6 +7,7 @@ String ProjectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
   sh "curl -u ${username}:${password} -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=${ProjectName}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json"
-  echo 'metrics collected'
+
 }
+  return metrics.json
 }
