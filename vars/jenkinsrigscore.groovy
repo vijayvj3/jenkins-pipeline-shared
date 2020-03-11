9f599526b741788f7794ffb48b4a50e1be6457f8
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-11 18:37:40 +0530

    Create jenkinsrigscore.groovy

---
+++ vars/jenkinsrigscore.groovy
@@ -0,0 +1,63 @@
+import groovy.json.*
+	
+
+def call(jsondata,github,jenkins,sonar){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+int ecount = jsonObj.riglet_info.auth_users.size()
+	def team=jsonObj.riglet_info.name
+List<String> jsonStringa= new ArrayList<String>();
+  jsonStringa.add(github)
+   jsonStringa.add(jenkins)
+	jsonStringa.add(sonar)
+   List<String> LIST = new ArrayList<String>();
+   for(i=0;i<jsonStringa.size();i++)
+  { 
+    int score=0
+    String name="  "
+	  String metric=" "
+if(jsonStringa[i].contains("gitlab"))
+    {
+    name="gitlab"
+    def jsonObj1 = readJSON text: jsonStringa[i]
+  def cnt =jsonObjc.gitlab.commit_cnt
+  LIST.add(["toolName":name,"metricName":"commits","value":cnt)
+   }
+   if(jsonStringa[i].contains("JENKINS"))
+    {
+      name="jenkins"
+      def jsonObjb = readJSON text: jsonStringa[i]
+	   // print jsonObjb
+      def total=jsonObjb.JENKINS.teambuild_cnt
+  def scnt =jsonObjb.JENKINS.teamsuccessbuild_cnt
+	    def fcnt=jsonObjb.JENKINS.teamfailurebuild_cnt
+	    LIST.add(["toolName":name,"metricName":"total_builds","value":total])
+	    LIST.add(["toolName":name,"metricName":"successful_builds","value":scnt])
+	    LIST.add(["toolName":name,"metricName":"failure_builds","value":fcnt])
+      }
+      if(jsonStringa[i].contains("Sonar"))
+    {
+	    name="sonar"
+	    def jsonObjc = readJSON text: jsonStringa[i]
+	    //print jsonObjc
+	    for(i=0;i<jsonObjc.Sonar.Metrics.component.measures.size();i++){
+		    //print jsonObjc.Sonar.Metrics.component.measures
+    def sonar_metric=jsonObjc.Sonar.Metrics.component.measures[i].metric
+		    def d=jsonObjc.Sonar.Metrics.component.measures[i].value
+    double data = Double.parseDouble(d); 
+       LIST.add(["toolName":name,"metricName":sonar_metric,"value":data])
+	    }
+    }
+     }
+def jsonBuilder = new groovy.json.JsonBuilder()
+
+jsonBuilder(
+ "teamName":team,
+  "metrics" : LIST
+  
+) 
+  
+  File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json")
+  file.write(jsonBuilder.toPrettyString())	
+	//def metrics1 = jsonBuilder.toString()
+}
