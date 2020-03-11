e5768f5147a411a801df6e8592fb3aeeadc2232a
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-11 20:27:30 +0530

    Update jenkinsrigscore.groovy

--- vars/jenkinsrigscore.groovy
+++ vars/jenkinsrigscore.groovy
@@ -21,7 +21,7 @@ if(jsonStringa[i].contains("gitlab"))
     name="gitlab"
     def jsonObj1 = readJSON text: jsonStringa[i]
   def cnt =jsonObjc.gitlab.commit_cnt
-  LIST.add(["toolName":name,"metricName":"commits","value":cnt)
+  LIST.add(["toolName":name,"metricName":"commits","value":cnt])
    }
    if(jsonStringa[i].contains("JENKINS"))
     {
