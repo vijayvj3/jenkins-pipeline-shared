472fa82eb92e9b9bfcb455dc8aa68e4a378cca1b
younu8084 <younu8084@gmail.com>
2020-03-11 19:04:34 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -8,10 +8,11 @@ def call(JSON,IP)
 def jsonString = JSON
 def jsonObj = readJSON text: jsonString
 def mailcount = jsonObj.riglet_info.auth_users.size()
-	def key= jsonObj.ci.projectplankey.key
-println(key)
+	println(mailcount)
+	def key1= jsonObj.ci.projectplankey.key
+println(key1)
 
-	sh "curl  -X GET  -u rig:rigaDapt@devOps '${IP}/rest/api/latest/result/${key}.json?max-result=50&expand=results.result.artifacts&expand=changes.change.files&start-index=0' -o outputbamboo.json"
+	sh "curl  -X GET  -u rig:rigaDapt@devOps '${IP}/rest/api/latest/result/${key1}.json?max-result=50&expand=results.result.artifacts&expand=changes.change.files&start-index=0' -o outputbamboo.json"
 	def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/outputbamboo.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
