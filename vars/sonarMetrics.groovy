65ed7306e76dfd5b3499b7afdacc22ee1609c753
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-11 17:42:06 +0530

    Update sonarMetrics.groovy

--- vars/sonarMetrics.groovy
+++ vars/sonarMetrics.groovy
@@ -5,8 +5,8 @@ def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 
-String a = jsonObj.code_quality.projects.project.project_name
-String ProjectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+
+String ProjectName=jsonObj.code_quality.projects.project.project_name
   
 withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
   sh "curl -u ${username}:${password} -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=${ProjectName}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json"
