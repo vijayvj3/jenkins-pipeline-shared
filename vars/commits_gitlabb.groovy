b7f9a96cd3544e5b387a6f3defb105aac17a11a3
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-12 16:04:23 +0530

    Update commits_gitlabb.groovy

--- vars/commits_gitlabb.groovy
+++ vars/commits_gitlabb.groovy
@@ -33,7 +33,7 @@ def jsonObj = readJSON text: jsonString
 	int ecount = jsonObj.riglet_info.auth_users.size()
          println("No of users "+ ecount)
       withCredentials([usernamePassword(credentialsId: 'gitlab_cred', passwordVariable: 'password', usernameVariable:'username')]) {
-	      sh "curl -X GET   -u $username:$password https://gitlab.com/api/v4/projects/${ids1}/repository/commits?limit=100 -o outputgitlab.json"
+	      sh "curl -X GET   -u $username:$password https://gitlab.com/api/v4/projects/${ids1}/repository/commits?per_page=100 -o outputgitlab.json"
       }
    def jsonSlurper = new JsonSlurper()
    def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/outputgitlab.json"),"UTF-8"))
