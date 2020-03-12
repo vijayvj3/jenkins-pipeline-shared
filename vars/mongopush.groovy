51c2855c923c37ed5859c89c46f9a9ec5b5ab2e1
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-12 10:51:59 +0530

    Update mongopush.groovy

--- vars/mongopush.groovy
+++ vars/mongopush.groovy
@@ -1,5 +1,5 @@
-def call()
+def call(IP)
 {
-	sh "curl -X POST --header 'Content-Type: application/json' --data @/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json  http://ec2-13-232-248-254.ap-south-1.compute.amazonaws.com:3000/api/metrics/teams/add"
+	sh "curl -X POST --header 'Content-Type: application/json' --data @/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json  '${IP}'/api/metrics/teams/add"
   echo "Sending data to Gamification API"
 }
